cc637dabe9a65a4dc11eafef215faf6211a67463
shobindiran <shobindiran@gmail.com>
2023-11-17 11:22:18 +0530

    changes made in catch

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //  common variables declarations
 const input = document.querySelector("#input-city");
 const searchForm = document.querySelector(".search");
-const apiKey = "26a1e74625a0488fa6c121139231611";
+const apiKey = "Your api key here"; //you can get api from www.weatherapi.com  
 const api = "https://api.weatherapi.com/v1/current.json?key=";
 
 // event listerner for search 
@@ -11,12 +11,12 @@ searchForm.addEventListener("submit",getData);
 async function getData(e){
     e.preventDefault();
 
-    let cityName = input.value;
-    let response = await fetch(api+apiKey+"&q="+cityName+"&aqi=no");
-    
+    let data;
     
     try{
-        let data = await response.json();
+        let cityName = input.value;
+        let response = await fetch(api+apiKey+"&q="+cityName+"&aqi=no");
+        data = await response.json();
 
         // feeding data to html elements
         document.querySelector("#city").innerText=data.location.name;
@@ -34,11 +34,22 @@ async function getData(e){
         document.querySelector(".weather").style.display="block";
     }
 
-    catch{
+    catch(error){
+        console.log(error);
+
         // removing the previous weather
         document.querySelector(".weather").style.display="none";
+        // condition to check blank submit
+        if(document.querySelector(".error>p").innerText = data.error.code==1003){
+            document.querySelector(".error>p").innerText ="Please enter a location";
+        }
+        else{  
+            document.querySelector(".error>p").innerText = data.error.message;
+        }
+
         // removing the current error
         document.querySelector(".error>p").style.display="block";
+        
     }
     
 }
